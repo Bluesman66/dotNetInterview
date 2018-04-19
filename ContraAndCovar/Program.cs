c554f009af0073399a08f15d609c093c8559eccd
alex.paclin <alex.paclin@gmail.com>
2018-04-19 18:52:20 +0500

    covariant & contravariant

---
+++ ContraAndCovar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContraAndCovar
+{
+    class Automobile
+    {
+
+    }
+
+    class Car : Automobile
+    {
+
+    }
+
+    class Truck : Automobile
+    {
+
+    }
+
+    class MonsterTruck : Truck
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            // Covariant example
+            IEnumerable<Automobile> covariant = new List<Truck>();
+
+            // Contravariant example
+            Action<Truck> contravariant = DescribeAutomobile;
+            contravariant(new Truck());
+            contravariant(new MonsterTruck());
+            //contravariant(new Car());
+
+            Action<Truck> contravariant2 = new Action<Automobile>((Automobile automobile) => { });
+
+            Console.ReadKey();
+        }        
+
+        static void DescribeAutomobile(Automobile automobile)
+        {
+            Console.WriteLine($"This automobile is a {automobile.GetType().Name}");
+        }
+    }
+}
---
+++ ContraAndCovar2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContraAndCovar2
+{
+    public class Animal
+    {
+        public void Eat() => Console.WriteLine("Eat");
+    }
+
+    public class Bird : Animal
+    {
+        public void Fly() => Console.WriteLine("Fly");
+    }
+
+    // Covariant delegates 
+    delegate Animal ReturnAnimalDelegate();
+    delegate Bird ReturnBirsDelegate();
+
+    // Contravariant delegates
+    delegate void TakeAnimalDelegate(Animal animal);
+    delegate void TakeBirdDelegate(Bird bird);
+
+    class Program
+    {
+        public static Animal GetAnimal() => new Animal();
+        public static Bird GetBird() => new Bird();
+
+        public static void Eat(Animal animal) => animal.Eat();
+        public static void Fly(Bird bird) => bird.Fly();
+        
+        static void Main(string[] args)
+        {
+            // Polymorphism
+            Animal a = new Bird();
+            // Covariant delegate
+            ReturnAnimalDelegate d = GetBird;
+
+            // Contravariant delegate
+            TakeBirdDelegate d2 = Eat;
+            d2(new Bird());
+        }
+    }
+}
